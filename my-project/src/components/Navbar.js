096a42c43f43302ed47e5fc78e399489749643d6
Dustin Pidcock <dustygen@gmail.com>
2021-02-25 20:36:41 -0800

    Starting the css of the home page

--- my-project/src/components/Navbar.js
+++ my-project/src/components/Navbar.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Yoo</h1>
-        </div>
-    )
-}
+  return (
+    <nav
+      className="flex justify-between iteem-center h-16 bg-white text-blacklist
+    relative shadow-sm font-mono"
+      role="navigation"
+    >
+      <Link to="/">Egg</Link>
+      <div className="px-4 cursor-pointer md:hidden">
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </div>
+      <div className="pr-8 md:block">
+        <Link className="p-4" to="/">
+          Home
+        </Link>
+        <Link className="p-4" to="/about">
+          About
+        </Link>
+        <Link className="p-4" to="/projects">
+          Projects
+        </Link>
+        <Link className="p-4" to="/contact">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
